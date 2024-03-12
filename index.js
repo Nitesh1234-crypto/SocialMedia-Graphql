6c4413bb03002c7a4792a28019bf79ee1ea672c1
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2024-03-12 11:44:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,9 @@ const mongoose = require('mongoose');
 const Post = require('./models/Post');
 
 
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
+const typeDefs = require("./graphql/typeDefs.js")
 
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
-  }
-};
+const resolvers = require("./graphql/resolvers/index.js")
 
 const server = new ApolloServer({
   typeDefs,
