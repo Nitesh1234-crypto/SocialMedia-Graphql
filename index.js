5fdd8345ddb900a44d72a8d532a0a042abbe8ca0
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2024-03-12 11:07:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 const Post = require('./models/Post');
-const { MONGODB } = require('./config.js');
+
 
 const typeDefs = gql`
   type Post {
